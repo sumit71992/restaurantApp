31bc331f037d5cbac2a345c41389ef7adf11849c
sumit71992 <sumit71992@gmail.com>
2023-05-07 00:59:18 +0530

    some bug fixed

--- src/components/Layout/HeaderCartButoon.js
+++ src/components/Layout/HeaderCartButoon.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import classes from './HeaderCartButton.module.css';
-import CartIcon from '../Cart/CartIcon';
+import 'font-awesome/css/font-awesome.min.css';
 
 function HeaderCartButoon() {
   return (
     <button className={classes.button}>
       <span className={classes.icon}>
-        <CartIcon />
+        <i className='fa fa-shopping-cart'></i>
       </span>
       <span>Your Cart</span>
       <span className={classes.badge}>0</span>
