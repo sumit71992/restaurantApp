f79bc477ad210de7145a3517cb74defb02ac0abe
sumit71992 <sumit71992@gmail.com>
2023-05-08 04:30:12 +0530

    making item visible some changes

--- src/components/Meals/MealItems.js
+++ src/components/Meals/MealItems.js
@@ -6,7 +6,7 @@ const MealItems = (props) => {
       <div>
         <h3>{props.name}</h3>
         <div className={classes.description}>{props.description}</div>
-        <div className={classes.price}>$ {props.price}</div>
+        <div className={classes.price}>$ {props.price.toFixed(2)}</div>
       </div>
     </li>
   );
